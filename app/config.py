af306b26392bb962a2d71cfc0acf046167c12b57
Accept-ernest <donkohernest2003@gmail.com>
2026-09-19 04:39:57 +0000

    Fix PostgreSQL driver for production

--- app/config.py
+++ app/config.py
@@ -6,9 +6,20 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
+database_url = os.getenv("DATABASE_URL")
+
+if database_url and database_url.startswith("postgresql://"):
+    database_url = database_url.replace(
+        "postgresql://",
+        "postgresql+psycopg://",
+        1
+    )
+
+
 class Config:
+
     SECRET_KEY = os.getenv("SECRET_KEY")
 
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = database_url
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
